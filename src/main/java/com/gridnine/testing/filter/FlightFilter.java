68a00608820c0587b3b02a9e47ac024e9d1204ad
den4os <118985600+den4os@users.noreply.github.com>
2023-10-02 16:47:33 +0300

    GT-1: added logic for the FlightFilter.java

--- src/main/java/com/gridnine/testing/filter/FlightFilter.java
+++ src/main/java/com/gridnine/testing/filter/FlightFilter.java
@@ -1,5 +1,36 @@
 package com.gridnine.testing.filter;
 
-public class FlightFilter {
+import com.gridnine.testing.model.Flight;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlightFilter {
+    private List<FlightFilterStrategy> filterStrategies;
+
+    public FlightFilter(List<FlightFilterStrategy> filterStrategies) {
+        this.filterStrategies = filterStrategies;
+    }
+
+    public List<Flight> filterFlights(List<Flight> flights) {
+
+        List<Flight> filteredFlights = new ArrayList<>();
+
+        for (Flight flight : flights) {
+            boolean isValid = true;
+
+            for (FlightFilterStrategy rule : filterStrategies) {
+                if (!rule.isFlightValid(flight)) {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid) {
+                filteredFlights.add(flight);
+            }
+        }
+
+        return filteredFlights;
+    }
 }
