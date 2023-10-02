57369846ed3060f2f4eef2cf2a2d2e0082016af0
den4os <118985600+den4os@users.noreply.github.com>
2023-10-02 20:49:09 +0300

    GT-4: added rules tests and minor fixes
    (cherry picked from commit b2dcb94dad3d17c9121743f8d946520b83154d5a)

--- src/test/java/com/gridnine/testing/filter/rules/SegmentsArrivalBeforeDepartureRuleTest.java
+++ src/test/java/com/gridnine/testing/filter/rules/SegmentsArrivalBeforeDepartureRuleTest.java
@@ -6,8 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -16,26 +15,48 @@ public class SegmentsArrivalBeforeDepartureRuleTest {
 
     private SegmentsArrivalBeforeDepartureRule rule;
     private LocalDateTime currentDateTime;
+    private boolean initializeRule = true;
 
     @Before
     public void setUp() {
         rule = new SegmentsArrivalBeforeDepartureRule();
-        currentDateTime = LocalDateTime.now();
+        if (initializeRule) {
+            currentDateTime = LocalDateTime.now();
+        }
     }
 
     @Test
-    public void testValidSegment() {
-        Segment validSegment = new Segment(currentDateTime.plusHours(1), currentDateTime.plusHours(2));
-        Flight flight = new Flight(List.of(validSegment));
+    public void testValidFlight() {
+        Segment validSegment1 = new Segment(currentDateTime.plusHours(1), currentDateTime.plusHours(2));
+        Segment validSegment2 = new Segment(currentDateTime.plusHours(3), currentDateTime.plusHours(4));
+        Flight validFlight = new Flight(Arrays.asList(validSegment1, validSegment2));
 
-        assertTrue(rule.isFlightValid(flight));
+        assertTrue(rule.isFlightValid(validFlight));
     }
 
     @Test
-    public void testInvalidSegment() {
-        Segment invalidSegment = new Segment(currentDateTime.plusHours(2), currentDateTime.plusHours(1));
-        Flight flight = new Flight(List.of(invalidSegment));
+    public void testInvalidFlight1() {
+        Segment invalidSegment1 = new Segment(currentDateTime.plusHours(1), currentDateTime.plusHours(4));
+        Segment invalidSegment2 = new Segment(currentDateTime.plusHours(2), currentDateTime.plusHours(6));
+        Flight invalidFlight = new Flight(Arrays.asList(invalidSegment1, invalidSegment2));
 
-        assertFalse(rule.isFlightValid(flight));
+        assertFalse(rule.isFlightValid(invalidFlight));
+    }
+
+    @Test
+    public void testInvalidFlight2() {
+        Segment invalidSegment1 = new Segment(currentDateTime.plusHours(3), currentDateTime.plusHours(4));
+        Segment invalidSegment2 = new Segment(currentDateTime.plusHours(1), currentDateTime.plusHours(2));
+        Flight invalidFlight = new Flight(Arrays.asList(invalidSegment1, invalidSegment2));
+
+        assertFalse(rule.isFlightValid(invalidFlight));
+    }
+
+    @Test
+    public void testEmptySegments() {
+        initializeRule = false;
+        Flight emptyFlight = new Flight(Arrays.asList());
+
+        assertFalse(rule.isFlightValid(emptyFlight));
     }
 }
