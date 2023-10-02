e2d31f2f834a5745a450a4642065d46c4347e33b
den4os <118985600+den4os@users.noreply.github.com>
2023-10-02 20:23:20 +0300

    GT-3: fixed SegmentsArrivalBeforeDepartureRule logic

---
+++ src/test/java/com/gridnine/testing/filter/rules/DepartureBeforeCurrentTimeRuleTest.java
@@ -0,0 +1,51 @@
+package com.gridnine.testing.filter.rules;
+
+import com.gridnine.testing.model.Flight;
+import com.gridnine.testing.model.Segment;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DepartureBeforeCurrentTimeRuleTest {
+
+    private DepartureBeforeCurrentTimeRule rule;
+    private LocalDateTime currentDateTime;
+    private boolean initializeRule = true;
+
+    @Before
+    public void setUp() {
+        rule = new DepartureBeforeCurrentTimeRule();
+        if (initializeRule) {
+            currentDateTime = LocalDateTime.now();
+        }
+    }
+
+    @Test
+    public void testValidFlight() {
+        Segment validSegment = new Segment(currentDateTime.plusHours(1), currentDateTime.plusHours(2));
+        Flight validFlight = new Flight(Arrays.asList(validSegment));
+
+        assertTrue(rule.isFlightValid(validFlight));
+    }
+
+    @Test
+    public void testInvalidFlight() {
+        Segment invalidSegment = new Segment(currentDateTime.minusHours(1), currentDateTime.plusHours(1));
+        Flight invalidFlight = new Flight(Arrays.asList(invalidSegment));
+
+        assertFalse(rule.isFlightValid(invalidFlight));
+    }
+
+    @Test
+    public void testEmptySegments() {
+        initializeRule = false;
+        Flight emptyFlight = new Flight(Arrays.asList());
+
+        assertFalse(rule.isFlightValid(emptyFlight));
+    }
+}
